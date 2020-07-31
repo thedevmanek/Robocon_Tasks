0f08a7045c6c3f713464a1ed0b7edf62d07f7bcd
thedevmanek <manekdev@outlook.com>
2020-08-01 00:02:50 +0530

    Fixed robbery.py

--- Linked_Lists/02/robbery.py
+++ Linked_Lists/02/robbery.py
@@ -1,22 +1,13 @@
-house_vaue = [6,7,1,3,8,2,4]
-# Odd houses
-def odd_houses():
-    i=0
-    sum=0
-    for i in range(0, len(house_vaue)):
-        if i%2 == 0:
-            pass
-        if i%2 != 0:
-            sum = sum + house_vaue[i]
-    return sum
-def even_houses():
-    i=0
-    sum=0
-    for i in range(0, len(house_vaue)):
-        if i%2 != 0:
-            pass
-        if i%2 == 0:
-            sum = sum + house_vaue[i]
-    return sum
+houses = [6, 7, 1, 3, 8, 2, 4]
 
-print(max(odd_houses(),even_houses()))
+
+def houseRobberRecursive(house_value):
+    def stealFromHouse(index):
+        if(index >= len(house_value)):
+            return 0
+
+        return max(house_value[index] + stealFromHouse(index + 2), stealFromHouse(index + 1))
+
+    return stealFromHouse(0)
+
+print(houseRobberRecursive(houses))
